27f92b2eac45e7a94fd06ea65281e20066021c85
Gary Dhillon <gary.dhil@gmail.com>
2019-01-25 15:45:51 -0700

    Added server functionality for the status requests, updated license and README with contributors and collaborators.

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Make a simple webserver.
 
 Contributors / Licensing
 ========================
+Consulted with:
+Kieter Lopez Philip Balisnomo: balisnom
+Shouyang Zhou: shouyang
+Owen Mcleod: omcleod
 
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
 
@@ -17,6 +21,7 @@ server.py contains contributions from:
 * Abram Hindle
 * Eddie Antonio Santos
 * Jackson Z Chang
+* Gary  Dhillon
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 #  coding: utf-8 
 import socketserver
+import os
+import mimetypes
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2019 Gary Dhillon
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -32,7 +35,69 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        response = self.parse(self.data)
+        self.request.sendall(bytearray(response,'utf-8'))
+
+    def parse(self, data):
+        data = data.decode()
+        splitData = data.split("\r\n")
+        
+        request = splitData[0]
+        splitRequest = request.split(" ")
+        requestType = splitRequest[0]
+        path = splitRequest[1]
+
+        headerTemplate = self.requestHandler(requestType, path)
+        
+        return headerTemplate
+
+    def requestHandler(self, requestType, path):
+        if requestType == "GET":
+            return self.contentHandler(path)
+        else:
+            errorCode = 405
+            return self.errorHandler(errorCode)
+
+    def errorHandler(self, errorCode):
+        if errorCode == 405:
+            statusCode = "405 Method not Allowed"
+        if errorCode == 404:
+            statusCode = "404 Not found"
+        errorTemplate = "HTTP/1.1 {fStatusCode}\r\n".format(fStatusCode=statusCode)
+        return errorTemplate
+
+    def contentHandler(self, path):
+        basePath = os.getcwd() + "/www"
+        serverPath = basePath + path
+        normalizedServerPath = os.path.realpath(serverPath)
+
+        if os.path.commonprefix([basePath, normalizedServerPath]) == basePath:
+            if os.path.exists(normalizedServerPath):
+                statusCode = "200 OK"
+                isDir = os.path.isdir(normalizedServerPath)
+
+                if isDir:
+                    dirServerPath = normalizedServerPath + "/index.html"
+                    contentType = mimetypes.guess_type(dirServerPath)
+                    contentFile = open(dirServerPath, "r")
+                else:
+                    contentType = mimetypes.guess_type(normalizedServerPath)
+                    contentFile = open(normalizedServerPath, "r")
+                
+                contentType = "Content-Type: " + contentType[0]
+                content = contentFile.read()
+                contentTemplate = "HTTP/1.1 {fStatusCode}\r\n{fContentType}\r\n\r\n\{fContent}".format(fStatusCode=statusCode, fContentType=contentType, fContent=content)                
+                contentFile.close()
+                return contentTemplate
+            else:
+                errorCode = 404
+                return self.errorHandler(errorCode)
+        else:
+            errorCode = 404
+            return self.errorHandler(errorCode)
+    
+    def redirectHandler(self):
+        pass
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
